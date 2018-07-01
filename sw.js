473d0e3d217d5ab120aaac5485f793833f49ecee
Victoria Offoma <offoma.victoria@gmail.com>
2018-07-01 22:33:35 +0100

    make url relative

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const cacheName = 'currency-converter';
 const filesToCache = [
-    '/',
-    '/index.html',
-    '/css/bootstrap.min.css',
-    '/css/style.css',
-    '/index.js',
-    '/lib/idb.js',
-    '/bootstrap.min.js',
-    '/services/dataService.js',
-    '/services/idbService.js'
+    './',
+    '.index.html',
+    './css/bootstrap.min.css',
+    './css/style.css',
+    './index.js',
+    './lib/idb.js',
+    './bootstrap.min.js',
+    './services/dataService.js',
+    './services/idbService.js'
 ];
 
 self.addEventListener('install', function(e){
@@ -26,6 +26,7 @@ self.addEventListener('activate', event => {
   });
   
   self.addEventListener('fetch', event => {
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
     event.respondWith(
         caches.match(event.request, {ignoreSearch: true}).then(response => {
             return response || fetch(event.request);
